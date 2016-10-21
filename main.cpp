be37ca247b13b9185611777c271f7d2d44b858ca
Samir Facussé <samir.facusse@unitec.edu>
2016-10-21 15:28:04 -0600

    ejercicio 1

--- main.cpp
+++ main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
-void ejercicio1();
+void ejercicio1(string);
 
 void ejercicio2(int);
 
 int encontrarPrimos(int);
 
-void ejercicio3();
+void ejercicio3(int);
 
 int main()
 {
@@ -17,15 +19,20 @@ int main()
 	{
 		cout<<"Escoja un ejercicio: ";
 		cin>>ejercicio;
+		int num;
+		string numero;
 		switch(ejercicio)
 		{
 			case 1:
 			{
+				cout<<"Ingrese el número: ";
+				cin>>numero;
+				ejercicio1(numero);
+				cout<<endl;
 				break;
 			}
 			case 2:
 			{
-				int num;
 				cout<<"Ingrese el número: ";
 				cin>>num;
 				ejercicio2(num);
@@ -34,11 +41,35 @@ int main()
 			}
 			case 3:
 			{
+				cout<<"Ingrese el número: ";
+				cin>>num;
+				ejercicio3(num);
 				break;
 			}
 		}
+		cout<<"Salir? [0=No/1=Si] ";
+		cin>> ejercicio;
 	}while(ejercicio == 0);
 	return 0;
+}
+
+void ejercicio1(string numero)
+{
+	int base;
+	int base_decimal;
+	int numeros[numero.size()+1];
+	for(int i = 0; i < numero.size(); i++)
+	{
+		numeros[i] = numero[i]-48;
+	}
+	int mayor = 0;
+	for(int j = 0; j < numero.size(); j++)
+	{
+		if(mayor<numeros[j])
+			mayor = numeros[j];
+	}
+	base = mayor+1;
+	
 }
 
 void ejercicio2(int num)
@@ -76,4 +107,9 @@ int encontrarPrimos(int numero_agregado)
 		return 1;
 	else
 		return 0;
+}
+
+void ejercicio3(int num)
+{
+
 }
