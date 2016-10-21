7a7779d43e00b518db158440ceb083b99e0280e4
Samir Facussé <samir.facusse@unitec.edu>
2016-10-21 14:39:52 -0600

    ejercicio 2

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@ void ejercicio1();
 
 void ejercicio2(int);
 
+int encontrarPrimos(int);
+
 void ejercicio3();
 
 int main()
@@ -27,6 +29,7 @@ int main()
 				cout<<"Ingrese el número: ";
 				cin>>num;
 				ejercicio2(num);
+				cout<<endl;
 				break;
 			}
 			case 3:
@@ -34,20 +37,43 @@ int main()
 				break;
 			}
 		}
-	}while(op == 0);
+	}while(ejercicio == 0);
 	return 0;
 }
 
 void ejercicio2(int num)
 {
+	int total = 0;
 	int divisores[20];
-	int posicion = 0;
-	for(int i = 1; i <= num; i++)
+	int posicion_1 = 0;
+	for(int i = 1; i <= num; i+=1)
 	{
-		if(num%i==0)
-		{
-			divisores[posicion] = i;
-			posicion++;
+		if(num%i==0){
+			divisores[posicion_1]=i;
+			posicion_1++;
 		}
 	}
+	for(int g = 0; g < posicion_1; g++)
+	{
+		if(encontrarPrimos((divisores[g]+num)/divisores[g]))
+		{
+			total+=(divisores[g]+num)/divisores[g];
+		}
+	}
+	cout<<"El total es: "<<total<<endl;
+
+}
+
+int encontrarPrimos(int numero_agregado)
+{
+	int divisores = 0;
+	for(int i = 1; i <= numero_agregado; i+=1)
+	{
+		if(numero_agregado%i==0)
+			divisores++;
+	}
+	if(divisores<=2)
+		return 1;
+	else
+		return 0;
 }
